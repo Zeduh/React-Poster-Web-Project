991069b097d2b31575fb84c45df9c61905544fb2
José Eduardo <jeduardocury@gmail.com>
2024-07-05 22:24:51 -0300

    Dynamic Routes

--- starting-project/src/main.jsx
+++ starting-project/src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import Posts, { loader as postsLoader } from './routes/Posts'
 import NewPost, { action as newPostAction } from './routes/NewPost'
+import PostDetails, { loader as postDetailsLoader} from './routes/PostDetails'
 import RootLayout from './routes/RootLayout'
 import './index.css'
 
@@ -22,6 +23,11 @@ const router = createBrowserRouter([
             element: <NewPost />,
             action: newPostAction
           },
+          {
+            path: '/:id',
+            element: <PostDetails />,
+            loader: postDetailsLoader
+          }
         ]},
   ] }
 ]);
